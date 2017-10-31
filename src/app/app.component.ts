6425c3ca34be42cf63b9091581a6f7734f037e80
Jan van Rossum <JvanRossum@snelstart.nl>
2017-10-31 16:19:50 +0100

    Spotify gebruiker data ophalen en weergeven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Http, RequestOptionsArgs, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
@@ -8,22 +10,42 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   private accessToken: string;
+  public name: string;
+  public imgSrc: string;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private http: Http
+  ) {}
   
   ngOnInit() {
     this.route.fragment.subscribe((fragment: string) => {
       if (fragment.startsWith("access_token=")) {
         let fragmentJson = this.getJsonFromFragmentParams(fragment);
         this.accessToken = fragmentJson.access_token;
+        this.getUserData();
       }
     })
   }
 
+  getUserData() {
+    let options: RequestOptionsArgs = {
+      headers: new Headers({
+        "Authorization": "Bearer " + this.accessToken,
+      })
+    };
+    this.http.get("https://api.spotify.com/v1/me", options)
+      .map((resp) => resp.json())
+      .subscribe((me: any) => {
+        this.name = me.display_name;
+        this.imgSrc = me.images[0].url;
+      });
+  }
+
   private getJsonFromFragmentParams(fragment: string): any {
-    var result = {};
-    fragment.split("&").forEach(function(part) {
-      var item = part.split("=");
+    let result = {};
+    fragment.split("&").forEach((part) => {
+      let item = part.split("=");
       result[item[0]] = decodeURIComponent(item[1]);
     });
     return result;
@@ -32,5 +54,9 @@ export class AppComponent implements OnInit {
   connectSpotify() {
     const clientId = "jouwClientID";
     location.href = "https://accounts.spotify.com/authorize?response_type=token&redirect_uri=http://localhost:4200&client_id=" + clientId;
+  }
+
+  isConnectedToSpotify() {
+    return this.accessToken != null;
   }
 }
