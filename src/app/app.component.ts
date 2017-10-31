113a05ece8bec8e6a84936c4c495490c3f5b6d77
Jan van Rossum <JvanRossum@snelstart.nl>
2017-10-31 14:29:54 +0100

    Vanuit applicatie autoriseren bij Spotify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  connectSpotify() {
+    const clientId = "jouwClientID";
+    location.href = "https://accounts.spotify.com/authorize?response_type=token&redirect_uri=http://localhost:4200&client_id=" + clientId;
+  }
 }
